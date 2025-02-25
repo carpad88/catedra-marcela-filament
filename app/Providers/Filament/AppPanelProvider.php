b559833f8a39519136532654a953bb42cf17dd74
Carlos Padilla <carpad88@gmail.com>
2025-02-24 23:55:41 -0600

    feat: change based font and add a footer with copyrights

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers\Filament;
 
 use App\Filament\App\Pages\Auth\RequestPasswordReset;
+use Blade;
 use DiogoGPinto\AuthUIEnhancer\AuthUIEnhancerPlugin;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
@@ -11,6 +12,7 @@ use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
+use Filament\View\PanelsRenderHook;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -32,11 +34,18 @@ class AppPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Neutral,
             ])
+            ->font('Fira Sans',
+                'https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap')
             ->brandLogo(asset('img/logo.svg'))
             ->brandLogoHeight('3rem')
             ->breadcrumbs(false)
             ->darkMode(false)
             ->topNavigation()
+            ->renderHook(
+                PanelsRenderHook::FOOTER,
+                fn (
+                ): string => Blade::render('<div class="px-6 py-8 text-xs text-gray-400 text-center">{{ now()->format("Y") }} © Marcela Ramírez | Todos los derechos reservados</div>'),
+            )
             ->discoverResources(in: app_path('Filament/App/Resources'), for: 'App\\Filament\\App\\Resources')
             ->discoverPages(in: app_path('Filament/App/Pages'), for: 'App\\Filament\\App\\Pages')
             ->pages([])
